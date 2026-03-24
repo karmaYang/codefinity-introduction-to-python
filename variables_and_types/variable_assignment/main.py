7f4cacc1c5584e1ae02c505f89221e0fad913307
karmaYang <>
2026-03-24 09:55:42 +0000

    Variable Assignment

--- variables_and_types/variable_assignment/main.py
+++ variables_and_types/variable_assignment/main.py
@@ -1,8 +1,10 @@
-
-
+item_name = "Eggs" # Assign a string for the item name
+item_price = 3.99  # Assign a float for the price per unit
+item_stock_quantity = 120  # Assign an integer for the quantity in stock
+in_stock = True  # Assign a boolean value indicating if eggs are in stock
 
 # Testing
-print("Item : ", "Eggs")
-print("Price per unit: $", 3.99)
-print("quantity in stock: ", 120)
-print("In stock: ", True)
+print("Item : ", item_name)
+print("Price per unit: $", item_price)
+print("quantity in stock: ", item_stock_quantity)
+print("In stock: ", in_stock)
