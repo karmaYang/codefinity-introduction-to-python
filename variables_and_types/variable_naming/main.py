ba68194a913130df877ef125b64a57b15e480ef9
karmaYang <>
2026-03-24 10:21:58 +0000

    Variable Naming

--- variables_and_types/variable_naming/main.py
+++ variables_and_types/variable_naming/main.py
@@ -1,7 +1,6 @@
-
-
-
-# Testing
-print("Item:", ___)
-print("Price per loaf: $", ___)
-print("Quantity in stock:", ___)
+item_name = "Bread"
+_item_price = 4.52
+#item quantity = 230
+item_in_stock = 230
+storename = "Green"
+print(item_name, _item_price, item_in_stock, storename)
